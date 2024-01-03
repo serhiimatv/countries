17a8f2e9babbfa31df47c9bdd934c82de0ba8d31
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2024-01-03 19:53:09 +0200

    init, header, darkmode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      white: '#FFFFFF',
+      black: '#111517',
+      HeaderDark: '#2B3844',
+      bodyLight: '#FAFAFA',
+      bodyDark: '#202C36',
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
